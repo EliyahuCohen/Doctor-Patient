1de85af27662c21babe50e9f17ced67c09e4c793
EliyahuCohen <100947769+EliyahuCohen@users.noreply.github.com>
2023-03-01 14:53:36 +0200

    small update in conversation page

--- client/src/pages/CommunicationPage/Communication.tsx
+++ client/src/pages/CommunicationPage/Communication.tsx
@@ -3,6 +3,10 @@ const Communication = () => {
   return (
     <div className="communication">
       <h1>Get In Touch 💬</h1>
+      <div className="sendingMessage">
+        <input type="text" />
+        <button>✉️</button>
+      </div>
     </div>
   );
 };
